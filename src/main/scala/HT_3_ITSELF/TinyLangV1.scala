641ce3f7724fde23498fe6049a65c877b2a308fa
d1md1m <kushnir.dmytro.93@gmail.com>
2017-05-30 01:24:32 +0300

    Basic Reduction Works. Issue to calculate Bools as well

--- src/main/scala/HT_3_ITSELF/TinyLangV1.scala
+++ src/main/scala/HT_3_ITSELF/TinyLangV1.scala
@@ -23,28 +23,43 @@ object TinyLangV1 {
       case _ => false
     }
 
-/*
+
+
     def reductionStep : Expr = this match {
-      case Sum(lOp: Expr, rOp: Expr)   => lOp.eval + rOp.eval
-      case Prod(lOp: Expr, rOp: Expr) => lOp.eval * rOp.eval
-      case Var(s: String) => env(s)
+      case bo:BinaryOper[_] =>
+        if (bo.lOp.isReduciable)
+          bo.sign match{
+            case " * "=> Prod(bo.lOp.reductionStep, bo.rOp)
+            case " + "=> Sum(bo.lOp.reductionStep, bo.rOp)
+            case " < "=> Less(bo.lOp.reductionStep, bo.rOp)
+          }
+        else if (bo.rOp.isReduciable)
+          bo.sign match{
+            case " * "=> Prod(bo.lOp,bo.rOp.reductionStep)
+            case " + "=> Sum(bo.lOp,bo.rOp.reductionStep)
+            case " < "=> Less(bo.lOp,bo.rOp.reductionStep)
+          }
+        else
+          Number(this.eval)
+
+      case Var(s: String) => Number(env(s))
     }
-*/
+
 
     def eval:Int  = this match {
       case Number(n) => n
-      case bo:BinaryOper[Int] => bo.map(bo.lOp.eval,bo.rOp.eval)
+      case bo:BinaryOper[Int] => bo.oper(bo.lOp.eval,bo.rOp.eval)
       case Var(s: String) => env(s)
     }
 
     def show: String = this match {
       case Number(n) => n.toString
-      case dj:Disj[_] => dj.lOp.show + dj.opStr + dj.rOp.show
+      case dj:Disj[_] => dj.lOp.show + dj.sign + dj.rOp.show
       case cj:Conj[_] => (cj.lOp, cj.rOp) match  {
-        case (djl:Disj[_], djr:Disj[_])  => "(" + cj.lOp.show + ")" + cj.opStr + "(" + cj.rOp.show +")"
-        case (djl:Disj[_], _ ) => "(" + cj.lOp.show + ")" + cj.opStr +  cj.rOp.show
-        case (_, djr:Disj[_] ) =>  cj.lOp.show  + cj.opStr + "(" + cj.rOp.show +")"
-        case (_, _ ) =>  cj.lOp.show  + cj.opStr + cj.rOp.show
+        case (djl:Disj[_], djr:Disj[_])  => "(" + cj.lOp.show + ")" + cj.sign + "(" + cj.rOp.show +")"
+        case (djl:Disj[_], _ ) => "(" + cj.lOp.show + ")" + cj.sign +  cj.rOp.show
+        case (_, djr:Disj[_] ) =>  cj.lOp.show  + cj.sign + "(" + cj.rOp.show +")"
+        case (_, _ ) =>  cj.lOp.show  + cj.sign + cj.rOp.show
       }
       case Var(s: String) => s
     }
@@ -54,8 +69,8 @@ object TinyLangV1 {
   trait BinaryOper[T] extends Expr {
     def lOp:Expr
     def rOp:Expr
-    def map(a:T, b:T):T
-    def opStr:String
+    def oper(a:T, b:T):T
+    def sign:String
   }
 
   trait Conj[T] extends BinaryOper[T]
@@ -67,17 +82,17 @@ object TinyLangV1 {
   }
 
   case class Number(n: Int) extends Unar[Int] {
-    override def Unit = 1
-    override def Nihil = 0
+    override def Unit:Int = 1
+    override def Nihil:Int = 0
   }
 
   case class Bool(b: Boolean) extends Unar[Boolean] {
-    override def Unit = true
-    override def Nihil = false
+    override def Unit:Boolean = true
+    override def Nihil:Boolean = false
   }
 
   case class Var(s: String) extends Unar[Any] {
-    override def Unit = AnyRef
+    override def Unit: Unit = AnyRef
     override def Nihil: Unit = None
   }
 
@@ -85,13 +100,13 @@ object TinyLangV1 {
   }
 
   case class Prod (lOp:Expr, rOp:Expr) extends Conj[Int]{
-    def map(a: Int, b: Int): Int = a * b
-    def opStr: String = " * "
+    def oper(a: Int, b: Int): Int = a * b
+    def sign: String = " * "
   }
 
   case class Sum (lOp:Expr, rOp:Expr) extends Disj[Int]{
-    def map(a: Int, b: Int): Int = a + b
-    def opStr: String = " + "
+    def oper(a: Int, b: Int): Int = a + b
+    def sign: String = " + "
   }
 
 
@@ -104,11 +119,10 @@ object TinyLangV1 {
   final class Machine {
 
     def run(expr: Expr):Expr = {
-      println(expr)
+      println(expr.show)
 
       if (expr.isReduciable)
-        expr
-      //    run (expr.reductionStep)
+        run (expr.reductionStep)
       else
         expr
     }
@@ -117,7 +131,7 @@ object TinyLangV1 {
 
 
   def main(args: Array[String]): Unit = {
-
+/*
     println(Sum(Number(5), Number(4)).show)
 
     println(Sum(Number(5), Number(4)).eval)
@@ -142,6 +156,12 @@ object TinyLangV1 {
 
     println( Prod(Number(5),
       Sum(Var("a"), Number(-3))).eval)
+*/
+
+    new Machine().run( Sum( Sum(Var("a"), Number(-3)) , Sum(Var("a"), Number(-3)) ))
+
+    new Machine().run(Prod(Number(5),
+      Sum(Var("a"), Number(-3))) )
   }
 
 }

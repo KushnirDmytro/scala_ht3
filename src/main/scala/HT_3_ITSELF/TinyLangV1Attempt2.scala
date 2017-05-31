2a2a0b654921d77c2d515aec7add2ef9485d3fbc
d1md1m <kushnir.dmytro.93@gmail.com>
2017-05-31 03:23:50 +0300

    Part1 Finished

--- src/main/scala/HT_3_ITSELF/TinyLangV1Attempt2.scala
+++ src/main/scala/HT_3_ITSELF/TinyLangV1Attempt2.scala
@@ -1,5 +1,8 @@
 package HT_3_ITSELF
 
+import IntSet.lec_6_IntegerSetTree.Empty
+
+import scala.runtime.Nothing$
 
 
 object TinyLangV1Attempt2 {
@@ -29,13 +32,21 @@ object TinyLangV1Attempt2 {
       case Number(n) => n
       case Sum(lOp: NumExpr, rOp: NumExpr) => lOp.eval + rOp.eval
       case Prod(lOp: NumExpr, rOp: NumExpr) => lOp.eval * rOp.eval
+      case _ => new Machine().run(this) match {
+        case Number(n:Int) => n
+        case _ => throw new Exception("Unexpected type (NOT INT) " + this.show )
+      }
     }
   }
 
   trait LogExpr extends Expr{
-    override def eval: Boolean = this match {
-      case Less(lOp: NumExpr, rOp: NumExpr) => lOp.eval < rOp.eval
-      case Bool(b:Boolean) => b
+    override def eval: Option[Boolean] = this match {
+      case Less(lOp: NumExpr, rOp: NumExpr) => Some[Boolean](lOp.eval < rOp.eval)
+      case Bool(b:Boolean) => Some[Boolean](b)
+      case _ => new Machine().run(this) match {
+        case Bool(b:Boolean) => Some[Boolean](b)
+        case _ => None //throw new Exception("Unexpected type (NOT BOOL) " + this.show )
+      }
     }
   }
 
@@ -62,6 +73,10 @@ object TinyLangV1Attempt2 {
     def sign = " < "
   }
 
+  case class IfElse(cond:LogExpr, lOp:Expr, rOp: Expr) extends Conj{
+    def sign = " : "
+  }
+
 
   trait Expr {
 
@@ -71,7 +86,7 @@ object TinyLangV1Attempt2 {
       case _ => false
     }
 
-    def OperReduce(l:Expr, r:Expr): Expr ={
+    def BinOpReduce(l:Expr, r:Expr): Expr ={
       this match {
         case ne:NumExpr => ne match {
           case Prod(_,_) => Prod(l, r)
@@ -80,7 +95,8 @@ object TinyLangV1Attempt2 {
         case le:LogExpr => le match  {
           case Less(_,_) =>  Less(l, r)
         }
-        case _ => ErrorExpr("LeftReduceError")
+        case IfElse(cond,_,_) => IfElse(cond,l, r)
+        case _ => ErrorExpr("BinOperReduceError")
       }
     }
 
@@ -89,8 +105,8 @@ object TinyLangV1Attempt2 {
       case bo:BinOper => bo match {
         case ne:NumExpr => (ne.lOp, ne.rOp) match {
           case (l:Expr,r:Expr) =>
-            if (l.isReduciable) OperReduce(l.reductionStep, r)
-            else if (r.isReduciable) OperReduce(l, r.reductionStep)
+            if (l.isReduciable) BinOpReduce(l.reductionStep, r)
+            else if (r.isReduciable) BinOpReduce(l, r.reductionStep)
             else (l,r) match {
               case (l:NumExpr, r:NumExpr) => Number(ne.eval)
               case (_,_) => println("NumericExpr [" +  ne.show + "] failed to simplify")
@@ -100,15 +116,28 @@ object TinyLangV1Attempt2 {
 
         case le:LogExpr=> le match {
           case Less(lOp: Expr, rOp: Expr) =>
-            if (lOp.isReduciable)  OperReduce(lOp.reductionStep, rOp)
-            else if (rOp.isReduciable ) OperReduce(lOp,rOp.reductionStep)
+            if (lOp.isReduciable)  BinOpReduce(lOp.reductionStep, rOp)
+            else if (rOp.isReduciable ) BinOpReduce(lOp,rOp.reductionStep)
             else (lOp, rOp) match {
-              case (lOp:NumExpr, rOp:NumExpr) => Bool(le.eval)
+              case (lOp:NumExpr, rOp:NumExpr) =>
+                val OptRez:Option[Boolean] = le.eval
+                if (OptRez.isDefined) Bool(OptRez.get) else ErrorExpr("LogicOperError")
               case _ =>
                 println("LogicalExpr [" +  le.show + "] failed to simplify")
                 ErrorExpr("LogicOperError")
             }
         }
+
+        case IfElse(cond, l, r) =>
+          if (l.isReduciable) BinOpReduce(l.reductionStep, r)
+          else if (r.isReduciable) BinOpReduce(l, r.reductionStep)
+          else cond.reductionStep match {
+            case rez:Bool => if (rez.value) l else  r
+            case _ => ErrorExpr("ErrorIfElseInferrence")
+          }
+
+
+
       }
 
       case Var(s: String) => env(s)
@@ -117,14 +146,15 @@ object TinyLangV1Attempt2 {
     }
 
 
-
       def eval: Any = this match {
+        case IfElse(cond, lOp, rOp) => this.reductionStep
         case Var(s: String) => env(s)
       }
 
 
       def show: String = this match {
         case un:UnarOper => un.value.toString
+        case IfElse(cond, l, r) => "{ (" + cond.show + ")_?_(" + l.show + ")_:_(" + r.show + ") }"
         case dj:Disj => dj.lOp.show + dj.sign + dj.rOp.show
         case cj:Conj => (cj.lOp, cj.rOp) match  {
           case (ldj:Disj, rdj:Disj)  => "(" + cj.lOp.show + ")" + cj.sign + "(" + cj.rOp.show +")"
@@ -201,6 +231,9 @@ object TinyLangV1Attempt2 {
 
     new Machine().run(Less(Less(Bool(false),
       Sum(Var("a"), Number(-3))), Number(2) ) )
+
+    new Machine().run( IfElse(Less(Number(2), Bool(false)), Number(-1) ,  Number(-1) ) )
+
 
   }
 
